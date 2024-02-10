96a3fc63810dafc8efdc65929ff6119445057840
0t4u <61939142+0t4u@users.noreply.github.com>
2024-02-10 01:58:51 +0000

    h

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
    input: 'src/index.tsx',
    output: [
       {
-         file: `dist_${manifest.name}.js`,
+         file: `dist/${manifest.name}.js`,
          format: 'cjs',
          strict: false
       },
